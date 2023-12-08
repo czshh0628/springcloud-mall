009600fd5714fa15c0dafb1a0be098125d35570a
czs <760620329@qq.com>
2023-12-08 10:40:54 +0800

    feat：deploy docs

---
+++ docs/src/.vuepress/config.ts
@@ -0,0 +1,16 @@
+import {defineUserConfig} from "vuepress";
+
+import theme from "./theme.js";
+
+export default defineUserConfig({
+    base: "/springcloud-mall/",
+    dest: "dist",
+    lang: "zh-CN",
+    title: "文档演示",
+    description: "vuepress-theme-hope 的文档演示",
+
+    theme,
+
+    // Enable it with pwa
+    // shouldPrefetch: false,
+});
